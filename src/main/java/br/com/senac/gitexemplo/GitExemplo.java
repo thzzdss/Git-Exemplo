d873bf8023c3d280503a637d3a5f9533b0823d5a
Thiago Santos <thiagods440@gmail.com>
2024-04-22 21:36:15 -0300

    Hello World

--- src/main/java/br/com/senac/gitexemplo/GitExemplo.java
+++ src/main/java/br/com/senac/gitexemplo/GitExemplo.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package br.com.senac.gitexemplo;
 
-/**
- *
- * @author diegu
- */
 public class GitExemplo {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World2!");
     }
 }
